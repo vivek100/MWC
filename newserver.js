eac01d7a1b8b854dc7d99f4e9a4de3eabc619a19
Vivek shukla <shukla.vivek1993@gmail.com>
2018-02-23 12:38:48 +0530

    Update newserver.js

--- newserver.js
+++ newserver.js
@@ -280,6 +280,5 @@ io.on('connection', function(socket){
            });
 });
 
-http.listen(8080, function(){
-    console.log('listening on *:8080');
-  });
+let port = process.env.PORT || 8080; 
+app.listen(port);
